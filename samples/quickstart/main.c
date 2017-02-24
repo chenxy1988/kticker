aee8832b93ae601e2652dcb67409266d9af768f8
Chen Xiangyu <xiangyu.chen@aol.com>
2017-02-24 16:42:30 +0800

    [KTCK0008][REBYPASS] update source

--- samples/quickstart/main.c
+++ samples/quickstart/main.c
@@ -2,7 +2,30 @@
 #include <string.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <time.h>  
+#include <sys/time.h>  
+#include <errno.h>  
+#include <string.h>  
+#include <unistd.h>
+#include <sys/select.h>  
+#include <sys/types.h>  
 #include "kticker.h"
+
+void x_sleep(unsigned int msec)
+{
+	struct timespec  tv;
+
+	if(msec > 999){
+		tv.tv_sec = msec/1000;
+		tv.tv_nsec = (msec * 1000 * 1000) - ((msec / 1000)*1000*1000*1000);
+	}else{
+		tv.tv_sec  = 0; /* Zero seconds */
+		tv.tv_nsec = (msec * 1000*1000);
+	}
+
+	nanosleep(&tv,NULL);
+}
+
 void *callback1(int argc,char **argv)
 {
 	printf("call 1 argc = %d,argv=%s\n",argc,argv[0]);
@@ -35,11 +58,11 @@ int main()
 		start_timer(tmr1,50,callback1,1,&h);
 		start_timer(tmr2,80,call2,0,NULL);
 		start_timer(tmr3,100,call3,0,NULL);
-		sleep(10);
+		x_sleep(50000);
 		delete_timer(tmr1);
-		sleep(20);
+		x_sleep(100000);
 		delete_timer(tmr2);
-		sleep(30);
+		x_sleep(300000);
 		delete_timer(tmr3);
 		c++;
 		printf("All timer delete 1s restart %d times\n",c);
